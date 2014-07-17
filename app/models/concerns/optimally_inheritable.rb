ed126a4a70ed0257b5f57d3d5a90de2e73ccc52d
Gaurab Paul <gaurab.paul@splashmath.com>
2014-07-17 18:20:45 +0530

    Second attempt

---
+++ app/models/concerns/optimally_inheritable.rb
@@ -0,0 +1,33 @@
+module OptimallyInheritable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def support_sti_for cls_list
+      @sti_cls_list = []
+      @sti_cls_list += cls_list
+    end
+
+    # TODO child class support
+    def sti_cls_list
+      @sti_cls_list
+    end
+
+    def find_sti_class type_name
+      idx = type_name.to_i
+      super if idx == 0
+      sti_cls_list[type_name.to_i-1].constantize
+    rescue NameError, TypeError
+      super
+    end
+
+    def sti_name
+      idx = sti_cls_list.index(self.name)
+      if idx.nil?
+        super
+      else
+        idx + 1
+      end
+    end
+
+  end
+end
--- app/models/institution/base.rb
+++ app/models/institution/base.rb
@@ -1,26 +1,4 @@
 class Institution::Base < ActiveRecord::Base
-
-  ALLOWED_CLASSES = %w[Institution::NonProfit Institution::Commercial]
-
-  class << self
-
-    def find_sti_class type_name
-      idx = type_name.to_i
-      super if idx == 0
-      ALLOWED_CLASSES[idx-1].constantize
-    rescue NameError, TypeError
-      super
-    end
-
-    def sti_name
-      idx = ALLOWED_CLASSES.index(self.name)
-      if idx.nil?
-        super
-      else
-        idx + 1
-      end
-    end
-
-  end
-
+  include OptimallyInheritable
+  support_sti_for %w[Institution::NonProfit Institution::Commercial]
 end
